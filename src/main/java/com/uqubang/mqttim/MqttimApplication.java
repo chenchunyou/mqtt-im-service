2d91615eb3ff4b423eeafc98ae0e072b93c6c863
chenchunyou <chenchunyou0407@163.com>
2021-04-16 12:02:29 +0800

    first commit

---
+++ src/main/java/com/uqubang/mqttim/MqttimApplication.java
@@ -0,0 +1,13 @@
+package com.uqubang.mqttim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uqubang/mqttim/MqttimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uqubang.mqttim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
